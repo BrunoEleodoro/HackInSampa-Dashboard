9cf779f4b023cbf1f750e9c19a890dbec98a3441
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-06-09 13:00:47 -0300

    a

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var express = require('express');
 var app = express();
 
 //setting middleware
-app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+app.use(express.static(__dirname + 'build')); //Serves resources from public folder
 
 
 var server = app.listen(process.env.PORT);
